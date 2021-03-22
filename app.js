8b70573d6fbebd0ce34fef370ed85d8b3293147e
Fiqri Dwi <nurfiqri23@gmail.com>
2021-03-22 09:55:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const burgerAnimation = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+};
+
+burgerAnimation();
